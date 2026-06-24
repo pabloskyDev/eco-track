9a9441ea3fbb129f8b619a89c6eaad07c203330f
Juan Pablo Betancur Martinez <jpbetancur1999@gmail.com>
2026-06-23 20:11:36 -0500

    feat: :sparkles: Se agrega sección de logs

--- src/app/ui/pages/logs/logs.page.ts
+++ src/app/ui/pages/logs/logs.page.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import {
+  IonButton,
+  IonCard,
+  IonCardContent,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonInput,
+  IonLabel,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
@@ -9,7 +16,45 @@ import {
 @Component({
   selector: 'app-logs',
   templateUrl: 'logs.page.html',
+  styleUrls: ['./logs.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+    IonIcon,
+    IonLabel,
+    IonInput,
+    IonCard,
+    IonCardContent,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LogsPage {}
+export class LogsPage {
+  selectedCategory: string = 'transport';
+  distance: string | null | undefined = null;
+  selectedMode: string = 'bus';
+
+  constructor() {}
+
+  setCategory(category: string) {
+    this.selectedCategory = category;
+  }
+
+  setMode(mode: string) {
+    this.selectedMode = mode;
+  }
+
+  saveActivity() {
+    const payload = {
+      category: this.selectedCategory,
+      distance: this.distance,
+      mode: this.selectedMode,
+    };
+    console.log('Actividad a guardar:', payload);
+    // Aquí iría tu lógica para enviar esto a un servicio/API
+  }
+}
